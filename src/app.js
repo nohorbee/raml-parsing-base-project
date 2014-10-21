220b4c70b6fed408fcad84fe52c78dec1f421758
Norberto L. Herz <norberto.herz@mulesoft.com>
2014-10-21 15:36:50 -0300

    little changes

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 var ramlParser = require('raml-parser');
 
 ramlParser.loadFile("./examples/example.raml").then(function(data) {
-  console.log(data);
+  console.log(JSON.stringify(data, null, 2));
 }) ;
